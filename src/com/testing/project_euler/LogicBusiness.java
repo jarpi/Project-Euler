28d4989e675577857fbfcbe6b9e16e908320c46d
jarpi <ebls123@gmail.com>
2014-05-23 23:11:30 +0200

    Problem 2 Euler

--- src/com/testing/project_euler/LogicBusiness.java
+++ src/com/testing/project_euler/LogicBusiness.java
@@ -19,31 +19,40 @@ public class LogicBusiness {
 		return (numberOfElements*(firstNumberInSequence+lastNumberInSequence))/2;  
 	} 
 	
+	public long SumOfFibonacciEvenNumbers() {
+		int n1=1, n2=1, temp=0; 
+		long sumOf = 0; 
+		do { 
+			if (n1%2==0) {
+				sumOf += n1; 
+			} 
+			temp = n2; 
+			n2 = n1; 
+			n1 = n1 + temp; 
+		} while (n1 < 4000000 && n2 < 4000000); 
+		return sumOf; 
+	} 
+	
 	static int mcd(int a, int b)
     {
         int x=0,nuevob=0;
         x = a;
  
-        if (a < b)
-        {
+        if (a < b) {
             a = b;
- 
             b = x;
             return mcd(a,b);
         }
-        else if (b != 0)
-        {
+        else if (b != 0) {
             nuevob = a % b;
             a = b;
             b = nuevob;
             return mcd(a,b);
         }
         return a;
- 
     }
  
-    static int mcm(int a, int b)
-    {
+    static int mcm(int a, int b) {
         int m=0;
         m=mcd(a,b);
         return(m*(a/m)*(b/m));
